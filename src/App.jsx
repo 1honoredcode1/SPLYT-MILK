d38255e5a82bea58eea5e88fc350041a0180d879
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-11-25 18:26:38 +0100

    le nav to the bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import NavBar from "./components/NavBar";
+
 const App = () => {
-  return <div className="text-red-600">App</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
